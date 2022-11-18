e62772adbd2207174f7b87c4bf1a0c8da0865081
Rushikesh Khosare Patil <52879589+rushipatil4444@users.noreply.github.com>
2022-11-18 18:57:08 +0530

    image final

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -1,7 +1,7 @@
 const Menu = [
   {
     id: 1,
-    image: "images/maggi.jpg",
+    image: "../Restraurant/images/maggi.jpg",
     name: "maggi",
     category: "breakfast",
     price: "12₹",
@@ -11,7 +11,7 @@ const Menu = [
 
   {
     id: 2,
-    image: "images/allupakoida.jpg",
+    image: "../Restraurant/images/allupakoida.jpg",
     name: "allu pakoida",
     category: "evening",
     price: "20₹",
@@ -20,7 +20,7 @@ const Menu = [
   },
   {
     id: 3,
-    image: "./images/corn.jpg",
+    image: "../Restraurant/images/corn.jpg",
     name: "corn",
     category: "breakfast",
     price: "10₹",
